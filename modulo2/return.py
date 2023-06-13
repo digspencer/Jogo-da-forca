9f4c2932292cbc85d685535be90e877bcef2af78
DiegoPaulino <diegoiglan@hotmail.com>
2023-06-13 06:37:55 -0300

    Entendendo o Return

---
+++ modulo2/return.py
@@ -0,0 +1,13 @@
+'''
+Retorno de valores das funções
+'''
+
+def soma(x, y):
+    return x + y
+
+
+soma1 = soma(2, 4)
+soma2 = soma(3, 3)
+
+#No caso com o 'return' ele retorna para dentro da função o valor
+print(soma1 + soma2)
