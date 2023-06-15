68776745ebfed8c6a915dc4a279f396647409c33
DiegoPaulino <diegoiglan@hotmail.com>
2023-06-15 06:53:10 -0300

    finalizando o closure

--- modulo2/closure.py
+++ modulo2/closure.py
@@ -1,10 +1,12 @@
 # Closure e funções que recebem outras funções
 
 def criar_saudacao(saudacao, nome):
-    return f'{saudacao},{nome}'
+    def saudar():
+        return f'{saudacao},{nome}'
+    return saudar
 
 s1 = criar_saudacao('Olá', ' Diego')
 s2 = criar_saudacao('Olá', ' Diego')
 
-print(s1)
-print(s2)
+print(s1()) # Aqui é onde ocorre o closure, chamando a função na hora da execução
+print(s2())
