c737c850d6ce8c3a8dafbe290fcfcc8510b89e57
DiegoPaulino <diegoiglan@hotmail.com>
2023-06-20 07:52:57 -0300

    Att

--- modulo2/aula207.py
+++ modulo2/aula207.py
@@ -1,17 +1,26 @@
 import json
 
-CAMINHO_ARQUIVO = 'Pessoas.json'
+CAMINHO_ARQUIVO = 'pessoa.json'
+
 
 class Pessoa:
     def __init__(self, nome, idade):
         self.nome = nome
         self.idade = idade
 
-p1 = Pessoa('João', 33)
-p2 = Pessoa('Helena', 11)
-p3 = Pessoa('Joana', 22)
 
+p1 = Pessoa('João', 33)
+p2 = Pessoa('Helena', 21)
+p3 = Pessoa('Joana', 11)
 bd = [vars(p1), p2.__dict__, vars(p3)]
 
-with open(CAMINHO_ARQUIVO, 'w') as arquivo:
-    json.dump(bd, arquivo, ensure_ascii=False, indent=2)
+
+def fazer_dump():
+    with open(CAMINHO_ARQUIVO, 'w') as arquivo:
+        print('FAZENDO DUMP')
+        json.dump(bd, arquivo, ensure_ascii=False, indent=2)
+
+
+if __name__ == '__main__':
+    print('ELE É O __main__')
+    fazer_dump()
