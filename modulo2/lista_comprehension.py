f2c018fcb76f82e712289c42112b67e6b6c4811d
DiegoPaulino <diegoiglan@hotmail.com>
2023-06-27 07:49:17 -0300

    Add Lista Comprehension

---
+++ modulo2/lista_comprehension.py
@@ -0,0 +1,15 @@
+# Introdução à List comprehension em Python
+# List comprehension é uma forma rápida para criar listas
+# a partir de iteráveis.
+
+lista = []
+
+for numero in range(10):
+    lista.append(numero)
+
+print(lista)
+
+# Lista Comprehension
+
+lista = [numero for numero in range(10)]
+print(lista)
