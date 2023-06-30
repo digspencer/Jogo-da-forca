43e5d71ad256d997e875bfabbb9c0b4d32b679f3
DiegoPaulino <diegoiglan@hotmail.com>
2023-06-30 07:38:57 -0300

    lambda

--- modulo2/LambdaFunction.py
+++ modulo2/LambdaFunction.py
@@ -1,5 +1,25 @@
-lista = [4, 3, 4, 5, 1, 2]
+# Introdução à função lambda (função anônima de uma linha)
+# A função lambda é uma função como qualquer
+# outra em Python. Porém, são funções anônimas
+# que contém apenas uma linha. Ou seja, tudo
+# deve ser contido dentro de uma única
+# expressão.
 
-# O método sort ordena a lista
-lista.sort()
-print(lista)
+lista = [
+    {'nome': 'Luiz', 'sobrenome': 'miranda'},
+    {'nome': 'Maria', 'sobrenome': 'Oliveira'},
+    {'nome': 'Daniel', 'sobrenome': 'Silva'},
+    {'nome': 'Eduardo', 'sobrenome': 'Moreira'},
+    {'nome': 'Aline', 'sobrenome': 'Souza'},
+]
+
+def exibir(lista):
+    for item in lista:
+        print(item)
+    print()
+
+l1 = sorted(lista, key=lambda item: item['nome'])
+l2 = sorted(lista, key=lambda item: item['sobrenome'])
+
+exibir(l1)
+exibir(l2)
